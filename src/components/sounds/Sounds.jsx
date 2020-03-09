b1be9550c2eab65993d93c3c3de078d035ee9fa4
Hanming Zhu <hanming@huawen.com>
2020-03-09 18:31:34 +0800

    fix: fix key issues with sounds

--- src/components/sounds/Sounds.jsx
+++ src/components/sounds/Sounds.jsx
@@ -20,19 +20,31 @@ const Sounds = () => {
   return (
     <>
       {bumpSounds.map(() => (
-        <ReactHowler src={bumpMp3} volume={0.7} />
+        <ReactHowler
+          src={bumpMp3}
+          volume={0.7}
+          key={`bump-sound-${Math.random()}`}
+        />
       ))}
       {selectSounds.map(() => (
-        <ReactHowler src={selectMp3} volume={0.5} />
+        <ReactHowler
+          src={selectMp3}
+          volume={0.5}
+          key={`select-sound-${Math.random()}`}
+        />
       ))}
       {correctSounds.map(() => (
-        <ReactHowler src={correctMp3} />
+        <ReactHowler src={correctMp3} key={`correct-sound-${Math.random()}`} />
       ))}
       {wrongSounds.map(() => (
-        <ReactHowler src={wrongMp3} />
+        <ReactHowler src={wrongMp3} key={`wrong-sound-${Math.random()}`} />
       ))}
       {menuSounds.map(() => (
-        <ReactHowler src={menuMp3} volume={0.7} />
+        <ReactHowler
+          src={menuMp3}
+          volume={0.7}
+          key={`menu-sound-${Math.random()}`}
+        />
       ))}
     </>
   );
